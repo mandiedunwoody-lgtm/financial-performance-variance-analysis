b51675d01ccd37e76bed767034efc353a19338fe
Mandie Dunwoody <mandiedunwoody@gmail.com>
2026-05-05 19:56:41 -0400

    Add variance calculations query to financials

--- sql/variance_calculations.sql
+++ sql/variance_calculations.sql
@@ -1,1 +1,10 @@
+SELECT
+    month,
+    year,
+    department,
+    cost_center,
+    actual_revenue - budget_revenue AS budget_variance,
+    actual_revenue - forecast_revenue AS forecast_variance,
+    (actual_revenue - budget_revenue) / NULLIF(budget_revenue,0) AS budget_variance_pct
+FROM financials;
 
