c807a2bf1d7bbd5d31bfac39f9ce710e091e62be
Mandie Dunwoody <mandiedunwoody@gmail.com>
2026-05-05 19:57:09 -0400

    Add KPI calculations for gross margin and forecast accuracy

--- sql/kpi_definitions.sql
+++ sql/kpi_definitions.sql
@@ -1,1 +1,9 @@
+SELECT
+    month,
+    year,
+    department,
+    cost_center,
+    (actual_revenue - actual_expense) / NULLIF(actual_revenue,0) AS gross_margin,
+    1 - ABS(actual_revenue - forecast_revenue) / NULLIF(actual_revenue,0) AS forecast_accuracy
+FROM financials;
 
